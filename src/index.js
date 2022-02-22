f1d8c0aa9cce72f2ae09964f4bcca2b926db1de9
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2022-02-22 12:55:47 +0900

    エスケープの方法を変更

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const exec = require('@actions/exec');
 
 async function DistributeAppCenter(token, path, app, mandatory, silent, distributionGroup, releaseNote)
 {
-	console.log(releaseNote);
 	await exec.exec(`appcenter distribute release --token ${token} -f ${path} -a ${app} -n ${github.context.runNumber} ${mandatory} ${silent} ${distributionGroup} ${releaseNote}`);
 }
 
@@ -13,7 +12,7 @@ async function Run()
 	try {
 		const mandatory = core.getBooleanInput('mandatory') ? '--mandatory' : '';
 		const silent = core.getBooleanInput('silent') ? '--silent' : '';
-		const releaseNote = core.getInput('release_notes') !== '' ? '-r \\\"' + core.getInput('release_notes') + '\\\"' : '';
+		const releaseNote = core.getInput('release_notes') !== '' ? `-r \"${core.getInput('release_notes')}\"` : '';
 		const group = core.getInput('group') !== '' ? `-g ${core.getInput('group')}` : ''
 
 		await DistributeAppCenter(
