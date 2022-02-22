5995a5f7a174f1015b55b732e1c8f39b8ad0166e
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2022-02-22 16:51:41 +0900

    \ では駄目だったので \" を入れる

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function Escape(text)
 		.replace(/</g, '\\x3c')
 		.replace(/>/g, '\\x3e')
 		.replace(/(0x0D)/g, '\r')
-		.replace(/(0x0A)/g, '\n')}\\`;
+		.replace(/(0x0A)/g, '\n')}\"`;
 }
 
 async function DistributeAppCenter(args)
