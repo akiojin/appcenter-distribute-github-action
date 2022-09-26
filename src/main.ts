ef5921d444cc3b514fd7a207686f73392721b180
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2022-09-26 16:11:50 +0900

    正規表現の間違い

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import * as tmp from 'tmp'
 
 function ReplaceInvalidChars(text: string): string
 {
-    return text.replace(/./g, '_').replace(/#/g, '_')
+    return text.replace(/[.]/g, '_').replace(/#/g, '_')
 }
 
 async function Run()
